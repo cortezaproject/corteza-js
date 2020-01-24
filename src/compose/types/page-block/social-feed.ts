8c910863db7a4ca1f834be64a54a56ec2fc472e6
Denis Arh <denis.arh@gmail.com>
2020-01-24 19:37:27 +0100

    Remove presentation functions

--- src/compose/types/page-block/social-feed.ts
+++ src/compose/types/page-block/social-feed.ts
@@ -37,53 +37,5 @@ export class PageBlockSocialFeed extends PageBlock {
   }
 }
 
-export function getTwitterHandle (url: string): string {
-  const twitterUnpacked = url.split('/')
-  // the fourth item should be the handle
-  if (twitterUnpacked.length === 4) {
-    return twitterUnpacked[3]
-  } else {
-    // something strange with this twitter url
-    return ''
-  }
-}
-
-export function extractSocialUrl (profileSourceField: string, profileUrl: string, record?: Record): object {
-  let url = ''
-  let socialNetwork = ''
-  let twitterHandle = ''
-  // check if we have a profileSourceField
-  if (profileSourceField && profileSourceField.length > 0 && record) {
-    const v = record.values[profileSourceField]
-    url = (Array.isArray(v) && v.length > 0 ? v[0] : v) as string
-  } else {
-    // see if we can fail back to profileUrl
-    if (profileUrl && profileUrl.length > 0) {
-      url = profileUrl
-    }
-  }
-
-  // is this a twitter url?
-  if (url && url.indexOf('twitter.com')) {
-    twitterHandle = getTwitterHandle(url)
-    if (twitterHandle === '') {
-      // failed to get twitter handle from the url
-      twitterHandle = ''
-      socialNetwork = ''
-    } else {
-      socialNetwork = 'Twitter'
-    }
-  } else {
-    // this was not a twitter url
-    url = ''
-    socialNetwork = ''
-  }
-
-  return {
-    url,
-    socialNetwork,
-    twitterHandle,
-  }
-}
 
 Registry.set(kind, PageBlockSocialFeed)
