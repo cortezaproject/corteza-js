83c87ec010806e420a02263bb18447f2f49e2ba9
Denis Arh <denis.arh@gmail.com>
2020-01-31 17:35:17 +0100

    Exporting Reminder type

--- src/system/index.ts
+++ src/system/index.ts
@@ -2,6 +2,7 @@ export { User } from './types/user'
 export { Role } from './types/role'
 export { Application } from './types/application'
 export { SinkRequest, SinkResponse } from './types/sink'
+export { Reminder } from './types/reminder'
 export {
   SystemEvent,
   RoleEvent,
