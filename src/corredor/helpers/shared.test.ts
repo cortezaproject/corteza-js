bd83b1f3c467e46803417ca722ab291f0b1a3f0b
Denis Arh <denis.arh@gmail.com>
2020-02-11 20:44:23 +0100

    Add tests for shared helper functions

---
+++ src/corredor/helpers/shared.test.ts
@@ -0,0 +1,47 @@
+import { describe, it, beforeEach } from 'mocha'
+import { expect } from 'chai'
+import sinon, { StubbedInstance, stubObject } from 'ts-sinon'
+import { extractID, genericPermissionUpdater } from './shared'
+import { NoID } from '../../cast'
+import { System as SystemAPI } from '../../api-clients'
+
+describe(__filename, () => {
+  beforeEach(() => {
+    sinon.restore()
+  })
+
+  describe('extractID', () => {
+    it('should extract the ID', () => {
+      expect(extractID(4200001)).to.equal('4200001')
+      expect(extractID('4200002')).to.equal('4200002')
+      expect(extractID({ prop: '4200003' }, 'prop')).to.equal('4200003')
+    })
+
+    it('should throw error on invalid input', () => {
+      expect(() => extractID('abc')).to.throw()
+      expect(() => extractID({ id: 'abc' }, 'id')).to.throw()
+    })
+
+    it('should extract 0', () => {
+      expect(extractID([]), 'array as ID should equal NoID').to.equal(NoID)
+      expect(extractID({}), 'empty object as ID should equal NoID').to.equal(NoID)
+      expect(extractID(''), 'empty string as ID should equal NoID').to.equal(NoID)
+      expect(extractID(), 'nothing as ID should equal NoID').to.equal(NoID)
+    })
+  })
+
+  describe('generic permission updater', () => {
+    it('should do group by role and make multiple calls to the API (one per role', async () => {
+      const API = stubObject<SystemAPI>(new SystemAPI({}))
+
+      await genericPermissionUpdater(API, [
+        { role: { roleID: '1111' } },
+        { role: { roleID: '1111' } },
+        { role: { roleID: '5555' } },
+        { role: { roleID: '5555' } },
+      ])
+
+      expect(API.permissionsUpdate.calledTwice).true
+    })
+  })
+})
