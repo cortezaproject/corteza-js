77bd1c329a13d26424a44100ed536571c149c621
Denis Arh <denis.arh@gmail.com>
2020-02-16 15:59:24 +0100

    Compose UI helper removed (moved to corteza-vue)

--- src/corredor/helpers/index.ts
+++ src/corredor/helpers/index.ts
@@ -1,4 +1,3 @@
 export { default as SystemHelper } from './system'
 export { default as ComposeHelper } from './compose'
-export { default as ComposeUIHelper } from './compose-ui'
 export { default as MessagingHelper } from './messaging'
