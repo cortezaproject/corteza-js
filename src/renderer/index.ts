882b46c94dcad3c506de44e1177d2ee61dbb5e26
Tomaž Jerman <tomaz.jerman@kendu.si>
2020-03-04 10:10:04 +0100

    Add V1 rendering system

---
+++ src/renderer/index.ts
@@ -0,0 +1,65 @@
+import { renderPDF, renderHTML } from './engines'
+import defaultStyle from './assets/defaultStyle'
+import { Render } from './util'
+
+const hps = require('html-parse-stringify')
+const ics = require('inline-css')
+const hm = require('html-minifier')
+
+export enum RendererKind {
+  PDF,
+  HTML,
+}
+
+export interface Document {
+  template: string;
+  data: { [key: string]: any };
+  renderer: RendererKind;
+  fontFace?: { [key: string]: any };
+}
+
+export interface Node {
+  type: string;
+  name?: string;
+  voidElement?: boolean;
+  attrs?: { [key: string]: any };
+  children?: Array<Node>;
+  content?: string;
+}
+
+/**
+ * render function renders the provided report object.
+ * @param document The report we want to render
+ */
+export async function render (document: Document): Promise<Render> {
+  // inline styles for easier processing
+  let template = `<style>${defaultStyle}</style>${document.template}`
+  template = await ics(template, {
+    url: ' ',
+    applyWidthAttributes: true,
+    applyTableAttributes: true,
+    removeHtmlSelectors: true,
+  })
+
+  // minify template to remove unneeded components such as whitespace, comments, ...
+  template = hm.minify(template, {
+    collapseWhitespace: true,
+    removeComments: true,
+    removeEmptyAttributes: true,
+  })
+
+  // parse template to an ast tree
+  const tree = hps.parse(template) as Array<Node>
+
+  // render the report
+  switch (document.renderer) {
+    case RendererKind.PDF:
+      return renderPDF(tree, document)
+
+    case RendererKind.HTML:
+      return renderHTML(tree, document)
+
+    default:
+      throw new Error('renderer.notSupported')
+  }
+}
