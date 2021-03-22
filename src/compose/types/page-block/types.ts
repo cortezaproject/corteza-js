ee73433e3c1bf6c0665f65423975c34c7f3fc2b3
Denis Arh <denis.arh@gmail.com>
2021-03-22 11:02:51 +0100

    Extend automation button struct to support workflow def

--- src/compose/types/page-block/types.ts
+++ src/compose/types/page-block/types.ts
@@ -1,15 +1,22 @@
-import { Apply } from '../../../cast'
+import { Apply, CortezaID } from '../../../cast'
 
 export class Button {
+  // Used when referring to Corredor automation script
   public script?: string = undefined
 
-  // resource type (copied from ui hook)
+  // Used when referring to workflow with onManual trigger
+  public workflowID?: string = undefined
+
+  // Used when referring to a specific step (triggered by onManual trigger)
+  public stepID?: string = undefined
+
+  // resource type (copied from ui hook or from trigger)
   public resourceType?: string = undefined;
 
   // Can override hook's label
   public label?: string = undefined;
 
-  // can override hooks's variant
+  // can override hook's variant
   public variant?: string = undefined;
 
   public enabled = true;
@@ -17,6 +24,7 @@ export class Button {
   constructor (b: Partial<Button>) {
     Apply(this, b, Boolean, 'enabled')
     Apply(this, b, String, 'label', 'variant', 'script', 'resourceType')
+    Apply(this, b, CortezaID, 'workflowID', 'stepID')
   }
 }
 
