1e107c33669fabe0642e9bd785c01926cf914827
Tomaž Jerman <tomaz.jerman121@gmail.com>
2021-06-27 13:10:41 +0200

    Tweak relay definitions based on the development changes

--- src/reporter/types/element.ts
+++ src/reporter/types/element.ts
@@ -1,7 +1,6 @@
 import { Apply } from '../../cast'
-import { DatasetDefinition } from './dataset'
 import { FrameDefinition } from './frame'
-import { Step, StepGroup, StepTransform } from './step'
+import { Step } from './step'
 import { RowDefinition } from './filter'
 
 export interface Element {
@@ -11,8 +10,7 @@ export interface Element {
   variant?: string;
   options?: unknown;
 
-  reportDefinitions?: (prefix: string) => { model: Array<Step>; frames: Array<FrameDefinition> };
-  elementKey: string;
+  reportDefinitions?: (prefix: string, model: Array<Step>) => { model: Array<Step>; frames: Array<FrameDefinition> };
 }
 
 interface SourceDefinition {
@@ -32,10 +30,6 @@ export class ElementText implements Element {
 
     this.kind = 'Text'
   }
-
-  get elementKey (): string {
-    return `[${this.name}]`
-  }
 }
 
 interface ChartOptions {
@@ -45,10 +39,10 @@ interface ChartOptions {
   labelColumn: string;
   dataColumns: Array<{ name: string; label?: string }>;
 
-  // things to transform the data
-  // @todo provide an array of these so we can control their order and stuff?
-  transform?: Partial<StepTransform>;
-  group?: Partial<StepGroup>;
+  // // things to transform the data
+  // // @todo provide an array of these so we can control their order and stuff?
+  // transform?: Partial<StepTransform>;
+  // group?: Partial<StepGroup>;
 }
 
 export class ElementChart implements Element {
@@ -78,7 +72,7 @@ export class ElementChart implements Element {
     this.options.dataColumns = o.dataColumns || []
   }
 
-  reportDefinitions (name: string): { model: Array<Step>; frames: Array<FrameDefinition> } {
+  reportDefinitions (name: string, model: Array<Step>): { model: Array<Step>; frames: Array<FrameDefinition> } {
     if (typeof this.options.source === 'object') {
       // @todo allow implicit sources
       throw new Error('chart source must be provided as a reference')
@@ -94,95 +88,6 @@ export class ElementChart implements Element {
       frames: [f],
     }
   }
-
-  // reportDefinitions (prefix: string): { model: Array<Step>; dataset: Array<DatasetDefinition> } {
-  //   const model: Array<Step> = []
-  //   const dataset: Partial<DatasetDefinition> = {}
-  //   const auxMM: Array<{ rootName: string; name: string; model: Array<Step>; dataset: Array<DatasetDefinition> }> = []
-  //   const oo = this.options || []
-
-  //   // - go over all of the chart definitions and prepare model steps
-  //   for (let i = oo.length - 1; i >= 0; i--) {
-  //     const opt = oo[i]
-  //     const m = this.modelChart(opt, prefix)
-  //     if (m) {
-  //       auxMM.push(m)
-  //     }
-  //   }
-
-  //   // @todo joins
-  //   const opt = oo[0]
-  //   // the most specific one; so the last one
-  //   const rootDef = auxMM[auxMM.length - 1]
-  //   model.push(...rootDef.model)
-
-  //   // prepare the dimension
-  //   //
-  //   // @todo paging, sorting, filtering
-  //   dataset.name = prefix + this.elementKey
-  //   dataset.dimension = rootDef.name
-  //   dataset.columns = [{ name: opt.labelColumn }].concat(opt.dataColumns.map(d => ({ ...d })))
-
-  //   return { model: model, dataset: [dataset as DatasetDefinition] }
-  // }
-
-  // private modelChart (opt: TableOptions, prefix: string): { model: Array<Step>; rootName: string; name: string; dataset: Array<DatasetDefinition> }|undefined {
-  //   const model: Array<Step> = []
-  //   let rootName = ''
-  //   let crtName = ''
-
-  //   // - things to load
-  //   if ((typeof opt.source) === 'object') {
-  //     const src = opt.source as SourceDefinition
-  //     rootName = opt.name
-  //     crtName = prefix + `[${rootName}]`
-
-  //     model.push(StepFactory({
-  //       load: {
-  //         name: crtName,
-  //         source: src.name,
-  //         definition: src.definition,
-  //         rows: src.rows,
-  //       },
-  //     }))
-  //   }
-
-  //   // - additional transformations in desc order
-  //   //   1. transform
-  //   //   2. group
-  //   if (opt.transform) {
-  //     const oldName = crtName
-  //     crtName = `transform(${crtName})`
-  //     model.push(StepFactory({
-  //       transform: {
-  //         name: crtName,
-  //         columns: opt.transform.columns,
-  //         dimension: oldName,
-  //         rows: opt.transform.rows,
-  //       },
-  //     }))
-  //   }
-
-  //   if (opt.group) {
-  //     const oldName = crtName
-  //     crtName = `group(${crtName})`
-  //     model.push(StepFactory({
-  //       group: {
-  //         name: crtName,
-  //         dimension: oldName,
-  //         columns: opt.group.columns,
-  //         groups: opt.group.groups,
-  //       },
-  //     }))
-  //   }
-
-  //   return {
-  //     dataset: [],
-  //     model: model,
-  //     name: crtName,
-  //     rootName,
-  //   }
-  // }
 
   get elementKey (): string {
     return `[${this.name}]`
@@ -200,14 +105,14 @@ interface TableOptions {
   source?: string|SourceDefinition;
   columns?: Array<TableColumn>;
 
-  // things to transform the data
-  // @todo provide an array of these so we can control their order and stuff?
-  transform?: Partial<StepTransform>;
-  group?: Partial<StepGroup>;
+  // // things to transform the data
+  // // @todo provide an array of these so we can control their order and stuff?
+  // transform?: Partial<StepTransform>;
+  // group?: Partial<StepGroup>;
 
-  // relationships define how the two tables are joined.
-  // the original table (local table) defines this.
-  relationships?: Array<{ column: string; refTable: string; refColumn: string }>;
+  // // relationships define how the two tables are joined.
+  // // the original table (local table) defines this.
+  // relationships?: Array<{ column: string; refTable: string; refColumn: string }>;
 
   striped: boolean;
   bordered: boolean;
@@ -271,154 +176,37 @@ export class ElementTable implements Element {
     this.options.columns = o.columns || []
   }
 
-  reportDefinitions (name: string): { model: Array<Step>; frames: Array<FrameDefinition> } {
+  reportDefinitions (name: string, model: Array<Step> = []): { model: Array<Step>; frames: Array<FrameDefinition> } {
     if (typeof this.options.source === 'object') {
       // @todo allow implicit sources
       throw new Error('table source must be provided as a reference')
     }
 
-    const f: FrameDefinition = {
-      name: this.name || name,
-      source: this.options.source,
-      // ref: '',
+    // when the source is joined, we need to send multiple frame definitions
+    const s = model.filter(({ join }) => !!join).find(({ join }) => join && join.name === this.options.source)
+    if (!s) {
+      return {
+        model: [],
+        frames: [{
+          name: this.name || name,
+          source: this.options.source,
+        }],
+      }
     }
 
     return {
       model: [],
-      frames: [f],
+      frames: [{
+        name: this.name || name,
+        source: this.options.source,
+        ref: s.join?.localSource,
+      }, {
+        name: this.name || name,
+        source: this.options.source,
+        ref: s.join?.foreignSource,
+      }],
     }
   }
-
-  //   switch (true) {
-  //     case this.variant === '' ||
-  //          this.variant === 'generic' ||
-  //          this.variant === 'stacked':
-  //       return this.definitionsGeneric(prefix)
-  //   }
-  //   throw new Error('unknown table variant ' + this.variant)
-  // }
-
-  // definitionsGeneric (prefix: string): { model: Array<Step>; dataset: Array<DatasetDefinition> } {
-  //   const model: Array<Step> = []
-  //   const dataset: Partial<DatasetDefinition> = {}
-  //   const auxMM: Array<{ rootName: string; name: string; model: Array<Step>; dataset: Array<DatasetDefinition> }> = []
-  //   const oo = this.options || []
-
-  //   // - go over all of the table definitions and prepare model steps
-  //   for (let i = oo.length - 1; i >= 0; i--) {
-  //     const opt = oo[i]
-  //     auxMM.push(this.modelTable(opt, prefix))
-  //   }
-
-  //   // - handle table joins
-  //   //
-  //   // Take the first definition as a base.
-  //   // Any table that needs to be included in the output must be referenced
-  //   // by the root table (the first one).
-  //   //
-  //   // @todo nested tables?
-  //   const opt = oo[0]
-  //   // the most specific one; so the last one
-  //   const rootDef = auxMM[auxMM.length - 1]
-  //   model.push(...rootDef.model)
-
-  //   // prepare the dimension
-  //   //
-  //   // @todo paging, sorting, filtering
-  //   dataset.name = prefix + this.elementKey
-  //   dataset.dimension = rootDef.name
-  //   dataset.columns = [...(oo[0].columns || [])]
-  //   if (opt.relationships) {
-  //     // only prefix columns if we will be joining things
-  //     dataset.columns = dataset.columns.map(({ name, ...rest }) => ({ ...rest, name: `${rootDef.name}.${name}` }))
-  //   }
-
-  //   for (const rel of opt.relationships || []) {
-  //     // find the related table
-  //     const relDef = auxMM.find(({ rootName }) => rootName === rel.refTable)
-  //     if (!relDef) {
-  //       throw new Error('unable to resolve the referenced table: ' + rel.refTable)
-  //     }
-
-  //     // add related model steps
-  //     model.push(...relDef.model)
-
-  //     // - add join definition
-  //     const name = `[${rootDef.name}:${rel.column}]+[${relDef.name}:${rel.refColumn}]`
-  //     model.push(StepFactory({
-  //       join: {
-  //         local: `${rootDef.name}.${rel.column}`,
-  //         foreign: `${relDef.name}.${rel.refColumn}`,
-  //         name,
-  //       },
-  //     }))
-  //     dataset.dimension = name
-
-  //     // - add the dataset def
-  //     const relOpt = oo.find(({ name }) => name === relDef.rootName)
-  //     dataset.columns.push(...(relOpt?.columns?.map(({ name, ...rest }) => ({ ...rest, name: `${relDef.name}.${name}` })) || []))
-  //   }
-
-  //   return { model: model, dataset: [dataset as DatasetDefinition] }
-  // }
-
-  // private modelTable (opt: TableOptions, prefix: string): { model: Array<Step>; rootName: string; name: string; dataset: Array<DatasetDefinition> } {
-  //   const model: Array<Step> = []
-  //   let rootName = ''
-  //   let crtName = ''
-
-  //   // - things to load
-  //   if ((typeof opt.source) === 'object') {
-  //     const src = opt.source as SourceDefinition
-  //     rootName = opt.name
-  //     crtName = prefix + `[${rootName}]`
-
-  //     model.push(StepFactory({
-  //       load: {
-  //         name: crtName,
-  //         source: src.name,
-  //         definition: src.definition,
-  //         rows: src.rows,
-  //       },
-  //     }))
-  //   }
-
-  //   // - additional transformations in desc order
-  //   //   1. transform
-  //   //   2. group
-  //   if (opt.transform) {
-  //     const oldName = crtName
-  //     crtName = `transform(${crtName})`
-  //     model.push(StepFactory({
-  //       transform: {
-  //         name: crtName,
-  //         columns: opt.transform.columns,
-  //         dimension: oldName,
-  //         rows: opt.transform.rows,
-  //       },
-  //     }))
-  //   }
-
-  //   if (opt.group) {
-  //     const oldName = crtName
-  //     crtName = `group(${crtName})`
-  //     model.push(StepFactory({
-  //       group: {
-  //         name: crtName,
-  //         dimension: oldName,
-  //         columns: opt.group.columns,
-  //         groups: opt.group.groups,
-  //       },
-  //     }))
-  //   }
-
-  //   return {
-  //     dataset: [],
-  //     model: model,
-  //     name: crtName,
-  //     rootName,
-  //   }
-  // }
 
   get elementKey (): string {
     return `[${this.name}]`
--- src/reporter/types/projection.ts
+++ src/reporter/types/projection.ts
@@ -1,27 +1,23 @@
 import { reporter } from '../..'
 import { Apply } from '../../cast'
 import { Element, ElementFactory } from './element'
-import { RowDefinition } from './filter'
 
-const defaultXYWH = [0, 0, 3, 3]
+const defaultXYWH: () => [number, number, number, number] = () => [0, 0, 5, 5]
 
 export class Projection {
   public title = ''
   public description = ''
   public layout = 'horizontal'
-  // The key is used when generating the model names.
-  // The key is automatically implied if not provided.
-  public key = ''
   public elements: Array<Element> = []
 
   public sources: Array<reporter.Step> = []
 
-  xywh: number[] = defaultXYWH
+  xywh: number[] = defaultXYWH()
 
   constructor (p: Partial<Projection>) {
     if (!p) return
 
-    Apply(this, p, String, 'title', 'description', 'layout', 'key')
+    Apply(this, p, String, 'title', 'description', 'layout')
 
     if (p.xywh) {
       if (!Array.isArray(p.xywh)) {
@@ -32,8 +28,7 @@ export class Projection {
         throw new Error('xywh must have 4 elements')
       }
 
-      // by default, park 3x3 block in upper left corner
-      this.xywh = p.xywh || defaultXYWH
+      this.xywh = p.xywh
     }
 
     this.elements = []
@@ -48,23 +43,5 @@ export class Projection {
     for (const s of p.sources || []) {
       this.sources.push(s as reporter.Step)
     }
-  }
-
-  // reportDefinitions provides the parameters extracted from the projections
-  // reportDefinitions (): { model: Array<Step>; dataset: Array<DatasetDefinition> } {
-  //   const model: Array<Step> = []
-  //   const dataset: Array<DatasetDefinition> = []
-
-  //   for (const e of this.elements) {
-  //     const d = e.reportDefinitions(this.projectionKey)
-  //     model.push(...d.model)
-  //     dataset.push(...d.dataset)
-  //   }
-
-  //   return { model, dataset }
-  // }
-
-  get projectionKey (): string {
-    return this.key
   }
 }
