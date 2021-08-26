fe49ce1c551e04e8d72f6fc953b00accf27f439b
Jože Fortun <joze.fortun@gmail.com>
2021-08-26 17:40:38 +0200

    Update basic report chart

--- src/reporter/types/display-elements/chart/basic.ts
+++ src/reporter/types/display-elements/chart/basic.ts
@@ -1,4 +1,5 @@
 import { ChartOptions, ChartOptionsRegistry } from './base'
+import { FrameDefinition, FrameColumn } from '../../frame'
 import { Apply } from '../../../../cast'
 
 export class BasicChartOptions extends ChartOptions {
@@ -15,6 +16,115 @@ export class BasicChartOptions extends ChartOptions {
     if (o.dataColumns) {
       this.dataColumns = o.dataColumns || []
     }
+  }
+
+  getChartConfiguration (dataframes: Array<FrameDefinition>) {
+    const config = {
+      type: this.type,
+      data: {
+        labels: this.getLabels(dataframes[0]),
+        datasets: this.getDatasets(dataframes[0], dataframes),
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+            },
+          }],
+        },
+        plugins: {
+          colorschemes: {
+            scheme: this.colorScheme,
+            reverse: true,
+          }
+        }
+      }
+    }
+
+
+    return config
+  }
+
+  getColIndex (dataframe: FrameDefinition, col: string) {
+    if (!dataframe || !dataframe.columns) return -1
+
+    return dataframe.columns.findIndex(({ name }) => name === col)
+  }
+
+  getLabels (localDataframe: FrameDefinition) {
+    const labels = []
+
+    if (this.labelColumn && localDataframe) {
+      const columnIndex = this.getColIndex(localDataframe, this.labelColumn)
+      if (columnIndex < 0) {
+        throw new Error(`Column ${this.labelColumn} not found`)
+      }
+
+      if (localDataframe.rows) {
+        for (const row of localDataframe.rows) {
+          labels.push(row[columnIndex])
+        }
+      }
+    }
+
+    return labels
+  }
+
+  getDatasets (localDataframe: FrameDefinition, dataframes: Array<FrameDefinition>) {
+    const chartDataset = []
+
+    if (this.dataColumns.length && localDataframe.rows) {
+      // Create dataset for each dataColumn
+      for (const { name } of this.dataColumns) {
+        // Assume localDataframe has the dataColumn
+        let columnIndex = this.getColIndex(localDataframe, name)
+
+        // If dataColumn is in localDataframe, then set that value
+        const data = localDataframe.rows.map(r => {
+          return columnIndex < 0 ? undefined : r[columnIndex]
+        })
+
+        // Otherwise check other dataframes for that columnn
+        if (columnIndex < 0) {
+          dataframes.slice(1).forEach(df => {
+            const { relColumn = '', refValue = '' } = df
+
+            // Get column that is referenced by relColumn
+            const relColumnIndex = this.getColIndex(localDataframe, relColumn)
+            if (relColumnIndex < 0) {
+              throw new Error(`Column ${relColumn} not found`)
+            }
+
+            if (!localDataframe.rows) {
+              throw new Error(`Local rows not found`)
+            }
+
+            // Get row index that matches refValue
+            const refRowIndex = localDataframe.rows.findIndex(row => row[relColumnIndex] === refValue)
+            if (refRowIndex < 0) {
+              throw new Error(`Row that matches refRowIndex ${refValue} not found`)
+            }
+
+            columnIndex = this.getColIndex(df, name)
+            if (columnIndex < 0) {
+              throw new Error(`Column ${name} not found`)
+            } else if (df.rows) {
+              data[refRowIndex] = df.rows[0][columnIndex]
+            }
+          })
+        }
+
+        chartDataset.push({
+          label: name,
+          data,
+        })
+      }
+    }
+
+    return chartDataset
   }
 }
 
