a15e4c9c95b2790a9ef7669d528013f42c70c338
Tomaž Jerman <tomaz.jerman121@gmail.com>
2021-09-29 10:09:38 +0200

    Fix circular dependancy issue

--- src/reporter/types/display-elements/chart/base.ts
+++ src/reporter/types/display-elements/chart/base.ts
@@ -1,9 +1,28 @@
 import { DisplayElement, DisplayElementInput, Registry } from '../base'
-import { ChartOptionsMaker } from './'
 import { FrameDefinition } from '../../frame'
 import { Apply } from '../../../../cast'
 
 const kind = 'Chart'
+
+export function ChartOptionsMaker<T extends ChartOptions> (options: Partial<ChartOptions>): T {
+  const { type } = options
+
+  if (type){
+    const ChartOptionsTemp = ChartOptionsRegistry.get(type)
+    if (ChartOptionsTemp === undefined) {
+      throw new Error(`unknown chart type '${type}'`)
+    }
+
+    if (options instanceof ChartOptions) {
+      // Get rid of the references
+      options = JSON.parse(JSON.stringify(options))
+    }
+
+    return new ChartOptionsTemp(options) as T
+  } else {
+    throw new Error(`no chart type`)
+  }
+}
 
 export type PartialChartOptions = Partial<ChartOptions>
 export type ChartOptionsInput = ChartOptions | PartialChartOptions
--- src/reporter/types/display-elements/chart/index.ts
+++ src/reporter/types/display-elements/chart/index.ts
@@ -1,28 +1,9 @@
-import { DisplayElementChart, ChartOptions, ChartOptionsRegistry } from './base'
+import { DisplayElementChart, ChartOptionsMaker, ChartOptionsRegistry } from './base'
 export { BasicChartOptions } from './basic'
 export { FunnelChartOptions } from './funnel'
 
-export function ChartOptionsMaker<T extends ChartOptions> (options: Partial<ChartOptions>): T {
-  const { type } = options
-
-  if (type){
-    const ChartOptionsTemp = ChartOptionsRegistry.get(type)
-    if (ChartOptionsTemp === undefined) {
-      throw new Error(`unknown chart type '${type}'`)
-    }
-
-    if (options instanceof ChartOptions) {
-      // Get rid of the references
-      options = JSON.parse(JSON.stringify(options))
-    }
-
-    return new ChartOptionsTemp(options) as T
-  } else {
-    throw new Error(`no chart type`)
-  }
-}
-
 export {
+  ChartOptionsMaker,
   ChartOptionsRegistry,
   DisplayElementChart,
 }
