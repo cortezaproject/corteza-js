e3a8ecde85b5c926e46ce155a1746f0e73cacb59
Jože Fortun <joze.fortun@gmail.com>
2022-09-05 13:16:39 +0200

    Fix number field display being limited to 3 decimals

--- src/formatting/number.ts
+++ src/formatting/number.ts
@@ -1,5 +1,5 @@
 import { currentLanguage } from './locale'
 
-export function number (input: number, options: Intl.NumberFormatOptions = {}): string {
+export function number (input: number, options: Intl.NumberFormatOptions = { maximumFractionDigits: 6 }): string {
   return new Intl.NumberFormat(currentLanguage(), options).format(input)
 }
