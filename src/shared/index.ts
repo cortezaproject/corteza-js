1b2204f5a507f58661ef518c203bcad6a7d3c188
Katrin Yordanova <katrin.yordanova@crust.tech>
2022-10-11 18:03:21 +0300

    Move coloschemes from chartjs-plugin-colorschemes into a dedicated folder and remove dependency

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,2 +1,3 @@
 export { Attachment } from './types/attachment'
-export { getColorschemeColors } from './types/chart/colorschemes'
+export { getColorschemeColors } from './types/chart/helper'
+export { default as colorschemes } from './types/chart/colorschemes'
