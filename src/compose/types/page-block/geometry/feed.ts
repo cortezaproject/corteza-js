beb02034428ef9140d37b43e4611b72cea4fa667
Kelani Tolulope <kelanik8@gmail.com>
2022-11-11 10:00:41 +0100

    Add support for toggling polygons in geometry configuration

--- src/compose/types/page-block/geometry/feed.ts
+++ src/compose/types/page-block/geometry/feed.ts
@@ -1,4 +1,3 @@
-import { feedResources } from './resources'
 import { Apply, NoID } from '../../../../cast'
 import { IsOf } from '../../../../guards'
 
@@ -10,6 +9,7 @@ interface FeedOptions {
   titleField: string;
   geometryField: string;
   displayMarker: boolean;
+  displayPolygon: boolean;
 }
 
 export type FeedInput = Partial<Feed> | Feed
@@ -21,7 +21,8 @@ const defOptions = {
   resource: 'compose:record',
   titleField: '',
   geometryField: '',
-  displayMarker: true,
+  displayMarker: false,
+  displayPolygon: true,
 }
 
 /**
@@ -32,7 +33,8 @@ export default class Feed {
   public titleField = ''
   public color = '#2f85cb'
   public geometryField = ''
-  public displayMarker = true
+  public displayMarker = false
+  public displayPolygon = true
   public options: FeedOptions = { ...defOptions }
 
   constructor (i?: FeedInput) {
@@ -44,7 +46,7 @@ export default class Feed {
 
     if (IsOf<Feed>(i, 'resource')) {
       Apply(this, i, String, 'resource', 'color', 'titleField', 'geometryField')
-      Apply(this, i, Boolean, 'displayMarker')
+      Apply(this, i, Boolean, 'displayMarker', 'displayPolygon')
 
       if (i.options) {
         this.options = { ...this.options, ...i.options }
